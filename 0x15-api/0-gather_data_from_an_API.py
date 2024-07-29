ec5e8765ed3b0d733c9d5f47b3ea92d61d63ebf0
root <root@DESKTOP-OB6IUIJ>
2024-07-29 23:08:24 +0300

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,9 @@
 import requests
 import sys
 
+
 def main(employee_id):
+    """endpoints"""
     # API URLs
     base_url = 'https://jsonplaceholder.typicode.com'
     employee_url = f'{base_url}/users/{employee_id}'
@@ -13,7 +15,7 @@ def main(employee_id):
     if response.status_code != 200:
         print("Employee not found")
         return
-    
+
     employee = response.json()
     employee_name = employee['name']
 
@@ -22,7 +24,7 @@ def main(employee_id):
     if response.status_code != 200:
         print("Error fetching TODO list")
         return
-    
+
     todos = response.json()
 
     # Filter TODO list for the given employee
@@ -30,19 +32,23 @@ def main(employee_id):
 
     # Calculate progress
     total_tasks = len(employee_todos)
-    done_tasks = len([todo for todo in employee_todos if todo['completed']])
-    completed_tasks_titles = [todo['title'] for todo in employee_todos if todo['completed']]
+    done_tasks = len([todo for todo in
+                      employee_todos if todo['completed']])
+    completed_tasks_titles = [todo['title']
+                              for todo in employee_todos if todo['completed']]
 
     # Output
-    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
+    print(f'Employee {employee_name}'
+          'is done with tasks({done_tasks}/{total_tasks}):')
     for title in completed_tasks_titles:
         print(f'     {title}')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
         main(employee_id)
